04ba1c4fdb3af1fc616ecfc0185f38d3bc3746e3
mleboeuf <ml@maximeleboeuf.com>
2017-10-18 08:11:03 -0400

    Add Server class

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-function Server(){
-	
+// Requires jQuery
+function Server(outputTarget, debugTarget){
+	// DOM element where to output the server document
+	this.outputTarget = outputTarget;
+
+	// DOM element where to output debug information
+	this.debugTarget = debugTarget;
 }
+
+Server.prototype.eventRaised = function(clientId, operation, position, value){
+	var debugLine = '<div class="debug-line">Event raised: Client' + clientId
+		+ ' ' + operation
+		+ ' @ ' + position
+		+ ' : "' + value
+		+ '"</div>';
+	this.debugTarget.append(debugLine);
+};
