a0f601ba05ad0566419773d9704cc5540ac5adeb
mleboeuf <ml@maximeleboeuf.com>
2017-10-19 07:16:46 -0400

    Server events

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ function Server(outputTarget, debugTarget){
 
 	// DOM element where to output debug information
 	this.debugTarget = debugTarget;
+
+	this.clients = [];
+	this.document = '';
 }
+
+Server.prototype.clientConnected = function(clientId){
+	var debugLine = '<div class="debug-line">Client ' + clientId + ' connected</div>'
+	this.debugTarget.append(debugLine);
+	this.clients.push(clientId);
+};
 
 Server.prototype.eventRaised = function(clientId, operation, position, value){
 	var debugLine = '<div class="debug-line">Event raised: Client' + clientId
@@ -14,4 +23,15 @@ Server.prototype.eventRaised = function(clientId, operation, position, value){
 		+ ' : "' + value
 		+ '"</div>';
 	this.debugTarget.append(debugLine);
+
+	if (operation == 'typeCharacter'){
+		this.document = [this.document.slice(0, position), value, this.document.slice(position)].join('');
+
+		// Show the state of the document in the server output window
+		this.outputTarget.text(this.document);
+	}
+};
+
+Server.prototype.broadcast = function(clientId, operation, position, value){
+
 };
